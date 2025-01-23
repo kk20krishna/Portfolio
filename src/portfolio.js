9fda6c3f59176febb8e44b37f6990ea03f610f7d
Krishna Kumar S <64156067+kk20krishna@users.noreply.github.com>
2025-01-23 17:29:02 +0000

    email changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -374,8 +374,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+91-9676645675",
-  email_address: "urkrishna@gmail.com"
+  number: "+91 967 664 5675",
+  email_address: "urkrishna at gmail dot com"
 };
 
 // Twitter Section
