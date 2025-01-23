5ccc1c7f11b370dbc55b733fcfd30f3532b47081
Krishna Kumar S <64156067+kk20krishna@users.noreply.github.com>
2025-01-23 17:38:11 +0000

    added kaggle

--- src/portfolio.js
+++ src/portfolio.js
@@ -38,7 +38,8 @@ const socialMediaLinks = {
   gmail: "urkrishna@gmail.com",
   //gitlab: "https://gitlab.com/saadpasta",
   //facebook: "https://www.facebook.com/saad.pasta7",
-  //medium: "https://medium.com/@saadpasta",
+  medium: "https://medium.com/@urkrishna",
+  kaggle: "https://www.kaggle.com/kk20krishna",
   //stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
